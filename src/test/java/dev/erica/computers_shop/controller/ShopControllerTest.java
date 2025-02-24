ca99a582daa1b30e8bb07a3b5fb220e57371f921
erikamc99 <erikamc99@gmail.com>
2025-02-24 20:56:16 +0100

    refactor test to add a before each and test removeComputer created and failed

--- src/test/java/dev/erica/computers_shop/controller/ShopControllerTest.java
+++ src/test/java/dev/erica/computers_shop/controller/ShopControllerTest.java
@@ -2,18 +2,34 @@ package dev.erica.computers_shop.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import dev.erica.computers_shop.models.*;
 
 public class ShopControllerTest {
+    private ShopController controller;
+    private ShopModel shop;
+
+    @BeforeEach
+    void setUp() {
+        shop = new ShopModel("Computer Shop", "Pepito", "123456789");
+        controller = new ShopController(shop);
+        controller.addComputer(new ComputerModel(Brand.APPLE, Memory.RAM_8GB, CPU.APPLE_M1, OS.MACOS, 1000));
+    }
+
     @Test
     @DisplayName("Test para añadir un ordenador desde el controlador")
     public void testAddComputer() {
-        ShopModel shop = new ShopModel("Computer Shop", "Pepito", "123456789");
-        ShopController controller = new ShopController(shop);
         controller.addComputer(new ComputerModel(Brand.ASUS, Memory.RAM_16GB, CPU.AMD_RYZEN5, OS.WINDOWS_10, 1500));
-        assertEquals(shop.computerList().size(), 1);
+        assertEquals(shop.computerList().size(), 2);
+    }
+
+    @Test
+    @DisplayName("Test para eliminar un ordenador desde el controlador")
+    public void testRemoveComputer() {
+        controller.removeComputer(Brand.APPLE, 1);
+        assertEquals(shop.computerList().size(), 0);
     }
 }
