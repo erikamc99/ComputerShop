b2b89b82070ba7e12a0bb3adb6ee9575e805668c
erikamc99 <erikamc99@gmail.com>
2025-02-24 18:37:38 +0100

    interfaz Computer created, computerModel created with constructor, setters and getters, test passed

---
+++ src/main/java/dev/erica/computers_shop/models/ComputerModel.java
@@ -0,0 +1,36 @@
+package dev.erica.computers_shop.models;
+
+
+public class ComputerModel implements Computer {
+    private final Brand brand;
+    private final Memory memory;
+    private final CPU CPU;
+    private final OS OS;
+    private double price;
+
+    public ComputerModel(Brand brand, Memory memory, CPU CPU, OS OS, double price) {
+        this.brand = brand;
+        this.memory = memory;
+        this.CPU = CPU;
+        this.OS = OS;
+        this.price = price;
+    }
+
+    @Override
+    public Brand getBrand() { return brand; }
+
+    @Override
+    public Memory getMemory() { return memory; }
+
+    @Override
+    public CPU getCPU() { return CPU; }
+
+    @Override
+    public OS getOS() { return OS; }
+
+    @Override
+    public double getPrice() { return price; }
+
+    @Override
+    public void setPrice(double price) { this.price = price; }
+}
