23f57b80841a722d518e6e4ae2c6aabf110d43c4
erikamc99 <erikamc99@gmail.com>
2025-02-24 18:46:37 +0100

    refactor test to add setPrice test and tostring, setPrice passed and toString failed

--- src/test/java/dev/erica/computers_shop/models/ComputerModelTest.java
+++ src/test/java/dev/erica/computers_shop/models/ComputerModelTest.java
@@ -3,15 +3,37 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class ComputerModelTest {
+
+    private ComputerModel computer;
+
+    @BeforeEach
+    void setUp() {
+        computer = new ComputerModel(Brand.ASUS, Memory.RAM_16GB, CPU.INTEL_I7, OS.WINDOWS_10, 1500.0);
+    }
+    
     @Test
     @DisplayName("Test del constructor del modelo de ordenador")
     void testComputerModel() {
-        ComputerModel computer = new ComputerModel(Brand.ASUS, Memory.RAM_16GB, CPU.INTEL_I7, OS.WINDOWS_10, 1500.0);
         assertEquals(Brand.ASUS, computer.getBrand());
         assertEquals(Memory.RAM_16GB, computer.getMemory());
         assertEquals(CPU.INTEL_I7, computer.getCPU());
         assertEquals(OS.WINDOWS_10, computer.getOS());
         assertEquals(1500.0, computer.getPrice());
+    }
+
+    @Test
+    @DisplayName("Test del método setPrice")
+    void testSetPrice() {
+        computer.setPrice(1600.0);
+        assertEquals(1600.0, computer.getPrice());
+    }
+
+    @Test
+    @DisplayName("Test del método toString")
+    void testToString() {
+        assertEquals("Ordenador: marca=ASUS, memoria=RAM_16GB, CPU=INTEL_I7, OS=WINDOWS_10, precio=1500.0€", computer.toString());
     }
 }
