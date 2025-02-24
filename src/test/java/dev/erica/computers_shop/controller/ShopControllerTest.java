42efc80fb5d02624052a368f98985714103e5da5
erikamc99 <erikamc99@gmail.com>
2025-02-24 20:43:37 +0100

    test to addComputer in controller created and failed

---
+++ src/test/java/dev/erica/computers_shop/controller/ShopControllerTest.java
@@ -0,0 +1,19 @@
+package dev.erica.computers_shop.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dev.erica.computers_shop.models.*;
+
+public class ShopControllerTest {
+    @Test
+    @DisplayName("Test para añadir un ordenador desde el controlador")
+    public void testAddComputer() {
+        ShopModel shop = new ShopModel("Computer Shop", "Pepito", "123456789");
+        ShopController controller = new ShopController();
+        controller.addComputer(new ComputerModel(Brand.ASUS, Memory.RAM_16GB, CPU.AMD_RYZEN5, OS.WINDOWS_10, 1500));
+        assertEquals(shop.computerList().size(), 1);
+    }
+}
